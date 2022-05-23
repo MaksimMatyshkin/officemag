b11c5f9a897e4560ee4a4a58f3af3cb85a15ab89
MaksimMatyshkin <98223167+MaksimMatyshkin@users.noreply.github.com>
2022-05-23 11:56:44 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,17 +1,19 @@
-sudo apt-get update
-sudo apt install vim -y
-sudo apt install wget -y
-sudo apt install htop -y
-sudo apt install tmux -y
-sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
-sudo curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
-sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-sudo apt-get update
-sudo apt-cache search php5
+sudo su
+apt-get update
+apt install vim -y
+apt install wget -y
+apt install htop -y
+apt install tmux -y
+apt-get -y install apt-transport-https lsb-release ca-certificates curl
+curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+apt-get update
+#apt-cache search php5
 sudo apt install php5.6 -y
+wget --quiet -O - http://nginx.org/keys/nginx_signing.key | apt-key add -
+echo "deb http://nginx.org/packages/debian/ $(lsb_release -sc) nginx">/etc/apt/sources.list.d/nginx.list
+echo "deb-src http://nginx.org/packages/debian/ $(lsb_release -sc) nginx">>/etc/apt/sources.list.d/nginx.list
+apt-get update
+apt-get install -y nginx
+systemctl start nginx   
 sudo apt install apache2 -y
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 8888 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -I INPUT 1 -i lo -j ACCEPT
-sudo iptables -A INPUT -j DROP
