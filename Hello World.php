937202636ae5bc23f5d7de2c27f501085e74260b
MaksimMatyshkin <98223167+MaksimMatyshkin@users.noreply.github.com>
2022-05-23 23:41:51 +0300

    Update Hello World.php

--- Hello World.php
+++ Hello World.php
@@ -6,3 +6,7 @@
  <?php echo '<p>Hello World!</p>'; ?>
  </body>
 </html>
+
+#или вот так сохранить файл с расширением hello world.php в корне и из браузера зайти http://127.0.0.1/hello world.php.
+
+<?php echo “Hello World!”; ?>
