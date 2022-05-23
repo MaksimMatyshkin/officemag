dac15b36b7bac49327843473da74513bf14a5402
MaksimMatyshkin <98223167+MaksimMatyshkin@users.noreply.github.com>
2022-05-23 11:05:46 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,13 @@
-sudo apt update
+sudo apt-get update
 sudo apt install vim -y
 sudo apt install wget -y
 sudo apt install htop -y
 sudo apt install tmux -y
+sudo apt-get -y install apt-transport-https lsb-release ca-certificates curl
+sudo curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt-get update
+sudo apt-cache search php5
 sudo apt install php5.6 -y
 sudo apt install apache2 -y
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
